8b54c932c9bc6f6a1ffc60b8ec9706e11a93897d
ghostyusheng <ghostyusheng@gmail.com>
2021-07-02 21:54:28 +0800

    支持远程调用

--- server.go
+++ server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strings"
 	"time"
 )
 
 func echo(conn *net.TCPConn) {
 	tick := time.Tick(5 * time.Second) // 五秒的心跳间隔
 	for now := range tick {
-		n, err := conn.Write([]byte(now.String() + "~~~~~"))
+		fmt.Println(now, " -> ")
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.Replace(cmd, "\n", "", -1)
+
+		n, err := conn.Write([]byte(cmd))
 		if err != nil {
 			log.Println(err)
 			conn.Close()
@@ -19,6 +26,9 @@ func echo(conn *net.TCPConn) {
 		fmt.Printf("send %d bytes to %s\n", n, conn.RemoteAddr())
 	}
 }
+
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
 	address := net.TCPAddr{
 		IP:   net.ParseIP("127.0.0.1"), // 把字符串IP地址转换为net.IP类型
@@ -34,7 +44,6 @@ func main() {
 			log.Fatal(err) // 错误直接退出
 		}
 		fmt.Println("remote address:", conn.RemoteAddr())
-		echo(conn)
-		//go echo(conn)
+		go echo(conn)
 	}
 }
